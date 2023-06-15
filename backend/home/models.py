04e27eba3e28f925b9fc95d58476a36e0ec6db36
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-15 16:38:38 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Azul(models.Model):
+    'Generated Model'
+    value = models.BigIntegerField()
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = models.CharField(max_length=256,)
